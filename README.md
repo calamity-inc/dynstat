74e1ac39f38f5f9780b0a818e81263fb9494409b
Sainan <63328889+Sainan@users.noreply.github.com>
2023-02-26 04:10:02 +0100

    Add minify option

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Develop dynamically, deploy statically.
 1. Add the [dynstat.yml](https://github.com/calamity-inc/dynstat-demo/blob/senpai/.github/workflows/dynstat.yml) to your .github/workflows folder.
 2. Set GitHub Pages "Source" to "Deploy from a branch", select the "gh-pages" branch, and click "Save."
 3. That's it!
+
+## Config
+
+You can configure dynstat with by creating `.dynstat.json` file. Any keys not provided are implied to be the default value:
+
+```JSON
+{
+	"minify": false
+}
+```
--- dynstat.php
+++ dynstat.php
@@ -1,4 +1,26 @@
 <?php
+// Load config
+$config = [];
+if (is_file(".dynstat.json"))
+{
+	$config = json_decode(file_get_contents(".dynstat.json"), true);
+}
+
+// Set config defaults
+$config["minify"] ??= false;
+
+// Setup minify engine if enabled
+if ($config["minify"])
+{
+	echo "Initialising minify engine...\n";
+	$minify_engine = file_get_contents("https://raw.githubusercontent.com/mecha-cms/x.minify/1276b3bf8a0de02fd49e01664c81b71178f3401c/engine/plug/minify.php");
+	$minify_engine = substr($minify_engine, 0, strpos($minify_engine, "\$state = "));
+	file_put_contents("minify_engine.php", $minify_engine);
+	require "minify_engine.php";
+	unlink("minify_engine.php");
+}
+
+echo "Making static buid...\n";
 if(!is_dir("build"))
 {
 	mkdir("build");
@@ -46,9 +68,29 @@ foreach(scandir(".") as $file)
 	{
 		$out_name = substr($out_name, 0, -4).".html";
 	}
-	file_put_contents($out_name, ob_get_contents());
 
+	$contents = ob_get_contents();
 	ob_end_clean();
+	if($config["minify"])
+	{
+		if(substr($out_name, -5) == ".html")
+		{
+			$contents = x\minify\f\minify_html($contents);
+		}
+		else if(substr($out_name, -4) == ".css")
+		{
+			$contents = x\minify\f\minify_css($contents);
+		}
+		else if(substr($out_name, -3) == ".js")
+		{
+			$contents = x\minify\f\minify_js($contents);
+		}
+		else if(substr($out_name, -5) == ".json")
+		{
+			$contents = x\minify\f\minify_json($contents);
+		}
+	}
+	file_put_contents($out_name, $contents);
 }
 
 unlink(".dynstat_runtime.php");
